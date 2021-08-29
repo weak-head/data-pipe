6d5ff1080b0ea088ce951c2934d0b88b0183e438
Oleksandr Zinchenko <zinchenko@live.com>
2021-08-29 17:40:13 -0400

    Entry point skeleton

--- cmd/data-pipe/main.go
+++ cmd/data-pipe/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+type cli struct {
+	cfg cfg
+}
+
+type cfg struct {
+}
+
+func (c *cli) initConfig(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func (c *cli) run(cmd *cobra.Command, args []string) error {
+
+	fmt.Print("Running")
+
+	return nil
+}
 
 func main() {
-	fmt.Print("Running")
+	cli := &cli{}
+	cmd := &cobra.Command{
+		Use:     "data-pipe",
+		PreRunE: cli.initConfig,
+		RunE:    cli.run,
+	}
+
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
